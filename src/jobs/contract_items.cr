d4aa18861cc180953ace2f8556a0d4ffb3025027
George Dietrich <george@dietrich.app>
2020-09-16 21:25:41 -0400

    Utilize work_pool for the contract items consumer

--- src/jobs/contract_items.cr
+++ src/jobs/contract_items.cr
@@ -5,7 +5,7 @@ struct EveShoppingAPI::Jobs::ContractItems
 
   def execute
     EveShoppingAPI::AMQPConnectionFactory.channel do |ch|
-      ch.basic_consume("contract.items", tag: "contract.items", block: true, no_ack: false) do |msg|
+      ch.basic_consume("contract.items", tag: "contract.items", block: true, no_ack: false, work_pool: 20) do |msg|
         contract_id = Int32.from_json msg.body_io
 
         Log.info { "Fetching items for contract #{contract_id}" }
